8334a796b18809ddc6f06ac97d14c8f95f5e5d19
Dev Pandey <dev.4.pandey@gmail.com>
2022-08-20 14:15:15 +0530

    chore: add eslint support in packages and apps dir

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,4 +1,20 @@
 module.exports = {
   root: true,
   extends: ["custom"],
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: "./tsconfig.json",
+  },
+  ignorePatterns: [
+    ".eslintrc.js",
+    "next.config.js",
+    "**/*.json",
+    "node_modules",
+    "public",
+    "styles",
+    ".next",
+    "coverage",
+    "dist",
+    ".turbo",
+  ],
 };
--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,8 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  extends: ["next", "airbnb", "airbnb-typescript", "turbo", "prettier"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
+    "react/react-in-jsx-scope": "off",
   },
 };
